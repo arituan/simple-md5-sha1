2cdb1a259adc669becf4a044daea1ce48c5b0158
arituan <arituan@gmail.com>
2018-05-10 21:35:44 +0700

    First Hash App

---
+++ Project/FirstHashApp/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace FirstHashApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbxDataFormat = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.tbData = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.tbSHA = new System.Windows.Forms.TextBox();
+            this.tbMD5 = new System.Windows.Forms.TextBox();
+            this.checkSHA = new System.Windows.Forms.CheckBox();
+            this.checkMD5 = new System.Windows.Forms.CheckBox();
+            this.fileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Data format :";
+            // 
+            // cbxDataFormat
+            // 
+            this.cbxDataFormat.FormattingEnabled = true;
+            this.cbxDataFormat.Items.AddRange(new object[] {
+            "File",
+            "String",
+            "Hex"});
+            this.cbxDataFormat.Location = new System.Drawing.Point(16, 30);
+            this.cbxDataFormat.Name = "cbxDataFormat";
+            this.cbxDataFormat.Size = new System.Drawing.Size(99, 21);
+            this.cbxDataFormat.TabIndex = 1;
+            this.cbxDataFormat.SelectedIndexChanged += new System.EventHandler(this.cbxDataFormat_SelectedIndexChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btnOpenFile);
+            this.panel1.Controls.Add(this.tbData);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.cbxDataFormat);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(422, 65);
+            this.panel1.TabIndex = 2;
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOpenFile.Location = new System.Drawing.Point(381, 29);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(24, 21);
+            this.btnOpenFile.TabIndex = 4;
+            this.btnOpenFile.Text = "...";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // tbData
+            // 
+            this.tbData.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbData.Location = new System.Drawing.Point(159, 30);
+            this.tbData.Name = "tbData";
+            this.tbData.Size = new System.Drawing.Size(216, 20);
+            this.tbData.TabIndex = 3;
+            this.tbData.TextChanged += new System.EventHandler(this.tbData_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(156, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Data:";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.btnReset);
+            this.panel2.Controls.Add(this.btnCalc);
+            this.panel2.Controls.Add(this.tbSHA);
+            this.panel2.Controls.Add(this.tbMD5);
+            this.panel2.Controls.Add(this.checkSHA);
+            this.panel2.Controls.Add(this.checkMD5);
+            this.panel2.Location = new System.Drawing.Point(12, 74);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(422, 180);
+            this.panel2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(347, 135);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(58, 40);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(250, 135);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(80, 40);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnCalc.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalc.Location = new System.Drawing.Point(16, 134);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(109, 40);
+            this.btnCalc.TabIndex = 4;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = false;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // tbSHA
+            // 
+            this.tbSHA.Location = new System.Drawing.Point(16, 107);
+            this.tbSHA.Name = "tbSHA";
+            this.tbSHA.ReadOnly = true;
+            this.tbSHA.Size = new System.Drawing.Size(389, 20);
+            this.tbSHA.TabIndex = 3;
+            // 
+            // tbMD5
+            // 
+            this.tbMD5.Location = new System.Drawing.Point(16, 45);
+            this.tbMD5.Name = "tbMD5";
+            this.tbMD5.ReadOnly = true;
+            this.tbMD5.Size = new System.Drawing.Size(389, 20);
+            this.tbMD5.TabIndex = 2;
+            // 
+            // checkSHA
+            // 
+            this.checkSHA.AutoSize = true;
+            this.checkSHA.Location = new System.Drawing.Point(16, 84);
+            this.checkSHA.Name = "checkSHA";
+            this.checkSHA.Size = new System.Drawing.Size(57, 17);
+            this.checkSHA.TabIndex = 1;
+            this.checkSHA.Text = "SHA-1";
+            this.checkSHA.UseVisualStyleBackColor = true;
+            // 
+            // checkMD5
+            // 
+            this.checkMD5.AutoSize = true;
+            this.checkMD5.Location = new System.Drawing.Point(16, 22);
+            this.checkMD5.Name = "checkMD5";
+            this.checkMD5.Size = new System.Drawing.Size(49, 17);
+            this.checkMD5.TabIndex = 0;
+            this.checkMD5.Text = "MD5";
+            this.checkMD5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 262);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MD5 and SHA-1 Hash";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbxDataFormat;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox tbSHA;
+        private System.Windows.Forms.TextBox tbMD5;
+        private System.Windows.Forms.CheckBox checkSHA;
+        private System.Windows.Forms.CheckBox checkMD5;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.TextBox tbData;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.OpenFileDialog fileDialog;
+    }
+}
+
---
+++ Project/FirstHashApp/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FirstHashApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            int typeData = cbxDataFormat.SelectedIndex;
+            byte[] data = null;
+
+            tbMD5.Text = string.Empty;
+            tbSHA.Text = string.Empty;
+
+            if (tbData.Text.Trim() == string.Empty)
+            {
+                MessageBox.Show("Data empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (checkMD5.Checked == false && checkSHA.Checked == false)
+            {
+                MessageBox.Show("Hash function does not choose", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (typeData == 0)
+            {
+                if (!File.Exists(tbData.Text.Trim()))
+                {
+                    MessageBox.Show("File does not exists", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                try
+                {
+                    data = File.ReadAllBytes(tbData.Text.Trim());
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+            }
+            else if (typeData == 1)
+            {
+                try
+                {
+                    data = Encoding.UTF8.GetBytes(tbData.Text);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+            }
+            else if (typeData == 2)
+            {
+                
+                try
+                {
+                    data = ConvertHexStringToByteArray(tbData.Text.Trim());
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+
+            if (checkMD5.Checked == true)
+            {
+                try
+                {
+                    tbMD5.Text = GetMd5Hash(data);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+            }
+
+            if (checkSHA.Checked == true)
+            {
+                try
+                {
+                    tbSHA.Text = GetSHA1Hash(data);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+            }
+
+
+        }
+
+        private byte[] ConvertHexStringToByteArray(string hexString)
+        {
+            if (hexString.Length % 2 != 0)
+            {
+                throw new ArgumentException(String.Format(CultureInfo.InvariantCulture, "The binary key cannot have an odd number of digits: {0}", hexString));
+            }
+
+            byte[] HexAsBytes = new byte[hexString.Length / 2];
+            for (int index = 0; index < HexAsBytes.Length; index++)
+            {
+                string byteValue = hexString.Substring(index * 2, 2);
+                HexAsBytes[index] = byte.Parse(byteValue, NumberStyles.HexNumber, CultureInfo.InvariantCulture);
+            }
+
+            return HexAsBytes;
+        }
+
+        private string GetMd5Hash(byte[] input)
+        {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                // Convert the input string to a byte array and compute the hash.
+                byte[] data = md5Hash.ComputeHash(input);
+
+                // Create a new Stringbuilder to collect the bytes
+                // and create a string.
+                StringBuilder sBuilder = new StringBuilder();
+
+                // Loop through each byte of the hashed data 
+                // and format each one as a hexadecimal string.
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+
+                // Return the hexadecimal string.
+                return sBuilder.ToString();
+            }
+        }
+
+        private string GetSHA1Hash(byte[] input)
+        {
+            using (SHA1 SHA1Hash = SHA1.Create())
+            {
+                // Convert the input string to a byte array and compute the hash.
+                byte[] data = SHA1Hash.ComputeHash(input);
+
+                // Create a new Stringbuilder to collect the bytes
+                // and create a string.
+                StringBuilder sBuilder = new StringBuilder();
+
+                // Loop through each byte of the hashed data 
+                // and format each one as a hexadecimal string.
+                for (int i = 0; i < data.Length; i++)
+                {
+                    sBuilder.Append(data[i].ToString("x2"));
+                }
+
+                // Return the hexadecimal string.
+                return sBuilder.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbxDataFormat.SelectedIndex = 0;
+        }
+
+        private void cbxDataFormat_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            tbData.Text = string.Empty;
+            if (cbxDataFormat.SelectedIndex != 0)
+            {
+                btnOpenFile.Hide();
+                tbData.Size = new Size(246, 20);
+            }
+            else
+            {
+                btnOpenFile.Show();
+                tbData.Size = new Size(216, 20);
+            }
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            fileDialog.ShowDialog();
+            if (fileDialog.FileName.Trim() != string.Empty)
+            {
+                tbData.Text = fileDialog.FileName.Trim();
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            cbxDataFormat.SelectedIndex = 0;
+            tbMD5.Text = string.Empty;
+            tbSHA.Text = string.Empty;
+            checkMD5.Checked = false;
+            checkSHA.Checked = false;
+            tbData.Text = string.Empty;
+        }
+
+        private void tbData_TextChanged(object sender, EventArgs e)
+        {
+            tbMD5.Text = string.Empty;
+            tbSHA.Text = string.Empty;
+        }
+    }
+}
